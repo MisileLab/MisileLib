d53017fad605d8df2d7a54760bce912acfd2378b
MisileLaboratory <misileminecord@gmail.com>
2022-06-11 02:52:08 +0900

    feat: :sparkles: add write config feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
 #[cfg(feature = "config")]
-use {std::{error::Error, fs::File, io::BufReader}, serde::Deserialize};
+use {std::{error::Error, fs::File, io::{BufReader, BufWriter}}, serde::{Deserialize, Serialize}};
 
 #[cfg(feature = "config")]
-pub fn read_user_from_file<T: for<'de> Deserialize<'de>>(file: &File) -> Result<T, Box<dyn Error>> {
+pub fn read_config<T: for<'de> Deserialize<'de>>(file: &File) -> Result<T, Box<dyn Error>> {
     let reader = BufReader::new(file);
     let c = serde_json::from_reader(reader)?;
 
     Ok(c)
+}
+
+#[cfg(feature = "config")]
+pub fn write_config<T: for<'de> Serialize>(config: &T, file: &File) -> Result<(), Box< dyn Error>> {
+    let writer = BufWriter::new(file);
+    Ok(serde_json::to_writer_pretty(writer, config)?)
 }
