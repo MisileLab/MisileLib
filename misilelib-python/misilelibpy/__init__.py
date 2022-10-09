4c68b05249d4d80bdfe891ccffabb36db7231de1
misilelaboratory <misileminecord@gmail.com>
2022-10-09 23:03:49 +0900

    add write_once and read_once

--- misilelib-python/misilelibpy/__init__.py
+++ misilelib-python/misilelibpy/__init__.py
@@ -5,3 +5,14 @@ def check_path(unixpath: str):
         return unixpath.replace('/', '\\')
     else:
         return unixpath
+
+def write_once(path: str, con: str):
+    a = open(path, 'w')
+    a.write(con)
+    a.close()
+
+def read_once(path: str):
+    a = open(path, 'r')
+    b = a.read()
+    a.close()
+    return b
