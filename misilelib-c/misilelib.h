d171ffaeb103bf3b6aae432e72d4fd7c814c5ea1
MisileLaboratory <icecreamhappytroll@gmail.com>
2023-01-04 03:25:17 +0000

    refactoring

--- misilelib-c/misilelib.h
+++ misilelib-c/misilelib.h
@@ -1,6 +1,6 @@
 
-char single_input(char* printstr);
-int int_input(char* printstr);
-float float_input(char* printstr);
-double double_input(char* printstr);
-char multiplechars(char* printstr);
+char single_input(char printstr[]);
+int int_input(char printstr[]);
+float float_input(char printstr[]);
+double double_input(char printstr[]);
+char* multiplechars(char printstr[]);
