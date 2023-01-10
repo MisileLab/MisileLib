1444ee0763a2ce3d0b9c56c871776999e303b99d
MisileLaboratory <icecreamhappytroll@gmail.com>
2023-01-10 03:12:56 +0000

    new patch bruh

--- misilelib-c/misilelib.c
+++ misilelib-c/misilelib.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
 
 char single_input(char printstr[]) {
     printf("%s", printstr);
@@ -28,11 +29,26 @@ double double_input(char printstr[]) {
     return retval;
 }
 
-void multiplechars(char printstr[], char* retval) {
+char multiplechars(char printstr[]) {
+    char* buffer;
     printf("%s", printstr);
-    scanf("%s", retval);
+    scanf("%s", buffer);
+    return *buffer;
 }
 
-bool compare_strs(char* char1[], char* char2[]) {
+bool compare_strs(char char1[], char char2[]) {
     return strcmp(char1, char2) == 0;
+}
+
+char* split (char buf[]) {
+    int i = 0;
+    char *p = strtok(buf, "/");
+    char *array[sizeof(p)];
+
+    while (p != NULL){
+        array[i++] = p;
+        p = strtok(NULL, "/");
+    }
+
+    return p;
 }
--- misilelib-c/misilelib.h
+++ misilelib-c/misilelib.h
@@ -1,7 +1,9 @@
+#include <stdbool.h>
 
 char single_input(char printstr[]);
 int int_input(char printstr[]);
 float float_input(char printstr[]);
 double double_input(char printstr[]);
-void multiplechars(char printstr[], char* retval);
-bool compare_strs(char* char1[], char* char2[]);
+char* multiplechars(char printstr[]);
+bool compare_strs(char char1[], char char2[]);
+char* split (char buf[]);
